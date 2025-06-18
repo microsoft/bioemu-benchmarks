3c772334452806f2b3133089e56d359705ac3dc1
Yu Xie <xieyu.fish@outlook.com>
2025-06-18 19:01:06 +0000

    fix scripts

--- bioemu_benchmarks/eval/folding_free_energies/utils.py
+++ bioemu_benchmarks/eval/folding_free_energies/utils.py
@@ -16,5 +16,8 @@ def load_reference(test_case: str) -> mdtraj.Trajectory:
         Loaded reference structure in mdtraj Trajectory format.
     """
     path = list(Path(Benchmark.FOLDING_FREE_ENERGIES.asset_dir).glob(f"**/{test_case}.pdb"))
+    if len(path) == 0:  # If no exact match, try to find a match by name without the mutant part.
+        name_wt = test_case.split("__")[0]
+        path = list(Path(Benchmark.FOLDING_FREE_ENERGIES.asset_dir).glob(f"**/{name_wt}.pdb"))
     assert len(path) == 1, f"Expected 1 reference structure for {test_case}, found {len(path)}."
     return mdtraj.load_pdb(path[0])
