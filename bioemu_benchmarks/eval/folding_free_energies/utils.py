f2677297469ac83b40c45d29aaefbf60f959f41f
Yu Xie <xieyu.fish@outlook.com>
2025-06-19 08:36:18 +0000

    always use wild type in load_ref

--- bioemu_benchmarks/eval/folding_free_energies/utils.py
+++ bioemu_benchmarks/eval/folding_free_energies/utils.py
@@ -8,6 +8,7 @@ from bioemu_benchmarks.benchmarks import Benchmark
 def load_reference(test_case: str) -> mdtraj.Trajectory:
     """
     Load reference structure based on test case ID and benchmark type.
+    Note that we load the wild type reference structure for mutants as well.
 
     Args:
         test_case: which test case to load the reference structure for.
@@ -15,9 +16,7 @@ def load_reference(test_case: str) -> mdtraj.Trajectory:
     Returns:
         Loaded reference structure in mdtraj Trajectory format.
     """
-    path = list(Path(Benchmark.FOLDING_FREE_ENERGIES.asset_dir).glob(f"**/{test_case}.pdb"))
-    if len(path) == 0:  # If no exact match, try to find a match by name without the mutant part.
-        name_wt = test_case.split("__")[0]
-        path = list(Path(Benchmark.FOLDING_FREE_ENERGIES.asset_dir).glob(f"**/{name_wt}.pdb"))
+    name_wt = test_case.split("__")[0]  # Get the wild type name from the test case ID.
+    path = list(Path(Benchmark.FOLDING_FREE_ENERGIES.asset_dir).glob(f"**/{name_wt}.pdb"))
     assert len(path) == 1, f"Expected 1 reference structure for {test_case}, found {len(path)}."
     return mdtraj.load_pdb(path[0])
